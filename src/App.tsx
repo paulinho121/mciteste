f3a090c577061d19a1b4fe985021c8d1457f2c20
paulofernandoautomacao <paulofernandoautomacao@gmail.com>
2025-09-18 16:13:53 +0000

    Fix: Força o fundo a ser branco

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ export default function App() {
 
   return (
     <ProtectedRoute>
-      <div className="min-h-screen bg-gray-50">
+      <div className="min-h-screen" style={{ backgroundColor: 'white' }}>
         <Header />
 
         <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
@@ -63,5 +63,3 @@ export default function App() {
     </ProtectedRoute>
   )
 }
-
-
